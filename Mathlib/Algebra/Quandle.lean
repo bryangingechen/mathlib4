6bdfac936892957ff85cf31e18551c3362cee55a
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2024-08-22 14:52:39 -0400

    break things again

--- Mathlib/Algebra/Quandle.lean
+++ Mathlib/Algebra/Quandle.lean
@@ -152,13 +152,13 @@ identity holds.
 -/
 lemma act_act_self_eq (x y : S) : (x ◃ y) ◃ x = x ◃ y := by
   have h : (x ◃ y) ◃ x = (x ◃ y) ◃ (x ◃ 1) := by rw [act_one]
-  rw [h, ← Shelf.self_distrib, act_one]
+  rw [h, ←Shelf.self_distrib, act_one]
 
 lemma act_idem (x : S) : (x ◃ x) = x := by rw [← act_one x, ← Shelf.self_distrib, act_one]
 
 lemma act_self_act_eq (x y : S) : x ◃ (x ◃ y) = x ◃ y := by
   have h : x ◃ (x ◃ y) = (x ◃ 1) ◃ (x ◃ y) := by rw [act_one]
-  rw [h, ← Shelf.self_distrib, one_act]
+  rw [h, ←Shelf.self_distrib, one_act]
 
 /--
 The associativity of a unital shelf comes for free.
