b086419fe6bc2de32e2f38e77765e4f6427721cc
adomani <adomani@gmail.com>
2024-11-08 14:25:35 +0100

    move source logic to `.sh` script

--- scripts/maintainer_merge_message.sh
+++ scripts/maintainer_merge_message.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-AUTHOR="${1}"   # adomani
-BODY="${2}"     # message content, containing `maintainer {merge, delegate}`
-GHsource="${3}" # one of `comment`, `review` or `review comment`
-PR="${4}"       # the number of the PR
-URL="${5}"      # the url link to the PR
-PR_TITLE="${6}" # the title of the PR
+AUTHOR="${1}"     # adomani
+BODY="${2}"       # message content, containing `maintainer {merge, delegate}`
+EVENT_NAME="${3}" # one of `issue_comment`, `pull_request_review` or `pull_request_review_comment`
+                  # to be converted to `comment`, `review` or `review comment`
+PR="${4}"         # the number of the PR
+URL="${5}"        # the url link to the PR
+PR_TITLE="${6}"   # the title of the PR
 
 mergeOrDelegate="neither merge nor delegate"
 if printf '%s\n' "${BODY}" | grep -q "^maintainer merge"
@@ -16,16 +17,31 @@ then
   mergeOrDelegate=delegate
 fi
 
+case ${EVENT_NAME} in
+  issue_comment)
+  SOURCE='comment'
+  ;;
+  pull_request_review)
+  SOURCE='review'
+  ;;
+  pull_request_review_comment)
+  SOURCE='review comment'
+  ;;
+  *)
+  SOURCE='unknown origin'
+  ;;
+esac
+
 GHevent=nothing
-if [ "${GHsource}" == "comment" ]
-then
-  GHevent=issue
-elif [ "${GHsource/% */}" == "review" ]
-then
-  GHevent=pull_request
-fi
+#if [ "${EVENT_NAME}" == "comment" ]
+#then
+#  GHevent=issue
+#elif [ "${EVENT_NAME/% */}" == "review" ]
+#then
+#  GHevent=pull_request
+#fi
 
-#printf $'title<<EOF\n${{ format(\'{0} requested a maintainer %s from %s on PR [#{1}]({2}):\', github.event.%s.user.login, github.event.%s.number, github.event.%s.html_url ) }}\nEOF' "${mergeOrDelegate}" "${GHsource}" "${GHsource}" "${GHevent}" "${GHevent}"
+#printf $'title<<EOF\n${{ format(\'{0} requested a maintainer %s from %s on PR [#{1}]({2}):\', github.event.%s.user.login, github.event.%s.number, github.event.%s.html_url ) }}\nEOF' "${mergeOrDelegate}" "${EVENT_NAME}" "${EVENT_NAME}" "${GHevent}" "${GHevent}"
 
-printf '%s requested a maintainer %s from %s on PR [#%s](%s):\n' "${AUTHOR}" "${mergeOrDelegate}" "${GHsource}" "${PR}" "${URL}"
+printf '%s requested a maintainer %s from %s on PR [#%s](%s):\n' "${AUTHOR}" "${mergeOrDelegate}" "${SOURCE}" "${PR}" "${URL}"
 printf '> %s\n' "${PR_TITLE}"
