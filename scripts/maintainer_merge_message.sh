d4567d78bd310dc940ae001fc8bd48e77e06c96e
adomani <adomani@gmail.com>
2024-11-09 17:21:56 +0100

    restructure script

--- scripts/maintainer_merge_message.sh
+++ scripts/maintainer_merge_message.sh
@@ -1,23 +1,12 @@
 #!/usr/bin/env bash
 
 AUTHOR="${1}"     # adomani
-BODY="${2}"       # message content, containing `maintainer {merge, delegate}`
+M_or_D="${2}"     # `merge` or `delegate`
 EVENT_NAME="${3}" # one of `issue_comment`, `pull_request_review` or `pull_request_review_comment`
                   # to be converted to `comment`, `review` or `review comment`
 PR="${4}"         # the number of the PR
 URL="${5}"        # the url link to the PR
 PR_TITLE="${6}"   # the title of the PR
-
-# figure out if the message contains `maintainer merge` or `maintainer delegate`
-# and set the `mergeOrDelegate` variable accordingly
-mergeOrDelegate="neither merge nor delegate"
-if printf '%s\n' "${BODY}" | grep -q "^maintainer merge"
-then
-  mergeOrDelegate=merge
-elif printf '%s\n' "${BODY}" | grep -q "^maintainer delegate"
-then
-  mergeOrDelegate=delegate
-fi
 
 # figure out if the GitHub event starting this action is a comment, a review or a review comment
 # and set the `SOURCE` variable accordingly
@@ -36,5 +25,5 @@ case ${EVENT_NAME} in
   ;;
 esac
 
-printf '%s requested a maintainer %s from %s on PR [#%s](%s):\n' "${AUTHOR}" "${mergeOrDelegate}" "${SOURCE}" "${PR}" "${URL}"
+printf '%s requested a maintainer %s from %s on PR [#%s](%s):\n' "${AUTHOR}" "${M_or_D}" "${SOURCE}" "${PR}" "${URL}"
 printf '> %s\n' "${PR_TITLE}"
