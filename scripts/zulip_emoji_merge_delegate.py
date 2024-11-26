779a605c8e1e2ef96145d658373b81b12997c2b0
adomani <adomani@gmail.com>
2024-11-26 19:23:36 +0000

    url

--- scripts/zulip_emoji_merge_delegate.py
+++ scripts/zulip_emoji_merge_delegate.py
@@ -30,7 +30,7 @@ response = client.get_messages({
     "num_after": 0,
     #"narrow": [{"operator": "channel", "operand": "PR reviews"}],
     #"narrow": [{"operator": "search", "operand": f'https://github.com/leanprover-community/mathlib4/pull/{PR_NUMBER}'}],
-    "narrow": [{"operator": "search", "operand": f'#{PR_NUMBER}'}],
+    "narrow": [{"operator": "search", "operand": f'github.com/leanprover-community/mathlib4/pull/{PR_NUMBER}'}],
 })
 
 messages = response['messages']
